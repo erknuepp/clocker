af3e23b9086af6136dfceeccdb9f8d314880bac4
Edward Knueppel <edward.knueppel@zywave.com>
2021-12-22 10:18:07 -0600

    Refactor

--- Statistics.cs
+++ Statistics.cs
@@ -3,16 +3,12 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     internal static class Statistics
     {
-        static TimeSpan Average(List<Lap> laps) 
+        public static TimeSpan Average(List<Lap> laps) 
         {
-            var tickses = laps.Select(x => x.Elapsed.Ticks);
-            var average = tickses.Average();
-            return new TimeSpan((long)average);        
+            return new TimeSpan((long)laps.Select(x => x.Elapsed.Ticks).Average());        
         }
     }
 }
